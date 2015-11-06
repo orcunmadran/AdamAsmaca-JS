54393c44c474822f3e85f93ab681aab0c9afb2b7
Orçun Madran <orcunmadran@gmail.com>
2015-11-06 22:38:48 +0300

    Genel çalışma mantığı tamam
    Seçilen kelimeye göre platform ve harf tarama hazırlandı.

--- adamAs.js
+++ adamAs.js
@@ -1,8 +1,27 @@
 /* Adam Asmaca  */
 /* 6 Kasım 2015 */
 
-var kelimeler = new Array("Kütüphane", "Koleksiyon", "Veri Tabanı", "Açık Erişim", "Otomasyon", "ISSN", "ISBN");
+var kelimeler = new Array('Kütüphane', 'Koleksiyon', 'Veri Tabanı', 'Açık Erişim', 'Otomasyon', 'ISSN', 'ISBN');
+var girilenHarf = 'a';
+//var secilenKelimeNo = Math.floor(Math.random() * 6);
+var secilenKelime = kelimeler[2];
+document.write('Seçilen Kelime: '+ secilenKelime +'<br>');
+var harfSayisi = secilenKelime.length;
+document.write('Harf Sayısı: '+ harfSayisi +'<br>');
 
-//var secilenKelimeno = Math.floor(Math.random() * 6);
-var secilenKelime = kelimeler[0];
-document.write(secilenKelime.indexOf("ü",2));
+var harfTutucu = new Array(harfSayisi);
+
+for(var j=0; j<harfSayisi; j++){
+    harfTutucu[j] = ' _ ';
+}
+
+document.write(harfTutucu);
+
+function harfKontrol(){
+    for(var i=0; i<harfSayisi; i++){
+        var bulunanHarfNo = secilenKelime.indexOf(girilenHarf,i);
+        harfTutucu[bulunanHarfNo] = girilenHarf;
+    }
+}
+harfKontrol();
+document.write('<br>' + harfTutucu);
