8d21da83e6812a397cf76588f39faee239b9ad9b
Orçun Madran <orcunmadran@gmail.com>
2015-11-06 15:43:56 +0300

    Adamı asıyoruz

---
+++ adamAs.js
@@ -0,0 +1,8 @@
+/* Adam Asmaca  */
+/* 6 Kasım 2015 */
+
+var kelimeler = new Array("Kütüphane", "Koleksiyon", "Veri Tabanı", "Açık Erişim", "Otomasyon", "ISSN", "ISBN");
+
+//var secilenKelimeno = Math.floor(Math.random() * 6);
+var secilenKelime = kelimeler[0];
+document.write(secilenKelime.indexOf("ü",2));
